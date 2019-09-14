cd695eb428c75365cba5bd4ebf8a4f85189926e5
RealEnt3 <49477961+RealEnt3@users.noreply.github.com>
2019-09-14 18:58:28 +0200

    Update var.bat

--- var.bat
+++ var.bat
@@ -1,3 +1,10 @@
+:: |*****************************|
+:: |Author:Gabriel Konrad
+:: |https://github.com/RealEnt3/
+:: | gabriel.konrad@gmx.net
+:: |by-: Gabriel Konrad
+:: |_____________________________|
+
 ::--[Main] - Based on \\dev\systemstart\\
     set passwort=BestPasswordEver
     set backdoor=recover
