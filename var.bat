b6b1721a3300dedbedaf232127c38465af0aa003
RealEnt3 <49477961+RealEnt3@users.noreply.github.com>
2019-09-14 18:44:55 +0200

    Update var.bat

--- var.bat
+++ var.bat
@@ -1,13 +1,4 @@
 ::--[Main] - Based on \\dev\systemstart\\
-    set passwort=bnderente
-    set online_name=KnusperEnte21
-    set real_name=Gabriel
-        REM "real_name" used by [profile_control] as "<Developer> Auth."
+    set passwort=BestPasswordEver
     set backdoor=recover
-    set restart_countdown=15
-::--Versions
-    set restart_version=1.0.2
-    set profile_control_version=1.2.1
-    set admin_cmd_version=1.0.0
-::--Create runas Dat. [TEMP] - \\dev\systemstart\\ - [profile_control]
-        set temp_runas_dat=%random%_temp
+    REM <Batch ignores large and lower case>
