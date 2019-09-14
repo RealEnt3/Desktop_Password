7f4bdfa38b62ef4ba513f1d75a184b820d4552da
RealEnt3 <49477961+RealEnt3@users.noreply.github.com>
2019-09-14 18:19:38 +0200

    Lockdown
    Save your Desktop with an password

---
+++ lockdown_desktop_pw_input.bat
@@ -0,0 +1,60 @@
+::exit
+::--Settings
+	@echo off
+		color 03
+			call %~d0\programmierung\dev\var.bat
+                title ## ENTER %real_name%s Desktop PASSWORD ##  
+                    taskkill /F /IM explorer.exe
+::                        start /min %~dp0%Backup_locked.bat
+                            cls 
+::--Time
+	@echo ###########
+	@echo ## %time:~0,5% ##
+	@echo ###########
+	@echo.
+::***********************************************************************
+    :pw
+        color 03
+        @echo ##################################
+        @echo ## Enter The Desktop - Password ##
+        @echo ##################################
+        @echo.
+        SET /P Input=Enter Password:
+        IF /I "%Input%"=="%backdoor%" goto dev_access
+        IF /I "%Input%"=="%passwort%" goto true	
+            goto false 
+::--False
+    :false
+        color 04
+            ping -n 1 localhost >NUL
+                @echo.
+                @echo ###################
+                @echo ## ACCESS DENIED ##
+                @echo ###################
+                @echo.
+                ping -n 2 localhost >NUL
+                @echo ERROR: Wrong Password, Please Try Again!
+                @echo.
+                ping -n 3 localhost >NUL
+                    goto pw 
+::--Dev.
+    :dev_access
+        @echo.
+        @echo ###########################
+        @echo ## Emergency Unlock Used ##
+        @echo ###########################
+            goto true
+::--True
+    :true
+        color 02
+            ping -n 1 localhost >NUL 
+                @echo.
+                @echo ####################
+                @echo ## ACCESS GRANTED ##
+                @echo ####################
+                @echo.
+                @echo Desktop Loading,... Please Wait
+                    ping -n 3 localhost >NUL
+                        start explorer.exe
+                            ping -n 2 localhost >NUL
+                            	exit
